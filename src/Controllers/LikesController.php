a6fe307d002970c6487157359f85a87c929f296a
Francisco Sepulveda <socieboy@gmail.com>
2015-06-12 14:59:21 -0700

    Unlike reply procedure updated to L5.1

--- src/Controllers/LikesController.php
+++ src/Controllers/LikesController.php
@@ -36,28 +36,15 @@ class LikesController extends Controller
      */
     public function like(LikeRequest $request, $slug)
     {
-
         $this->dispatchFrom('Socieboy\Forum\Jobs\LikeReply', $request);
-        //$like = $this->likeRepo->model();
-
-        //$manager = new LikeManager($like, $request->except('_token'));
-
-        //$manager->save();
 
         return redirect()->route('forum.conversation.show', $slug);
-
     }
-
 
     public function unlike(LikeRequest $request, $slug)
     {
-        $data = $request->only('reply_id');
 
-        $like = $this->likeRepo->where('reply_id', $data['reply_id'])
-                        ->where('user_id', Auth::User()->id)
-        ->get()->first();
-
-        $like->delete();
+        $this->dispatchFrom('Socieboy\Forum\Jobs\UnLikeReply', $request);
 
         return redirect()->route('forum.conversation.show', $slug);
 
--- src/Jobs/UnLikeReply.php
+++ src/Jobs/UnLikeReply.php
@@ -3,16 +3,21 @@
 namespace Socieboy\Forum\Jobs;
 
 use App\Jobs\Job;
+use Illuminate\Auth\Guard;
 use Illuminate\Contracts\Bus\SelfHandling;
+use Socieboy\Forum\Entities\Likes\LikeRepo;
 
 class UnLikeReply extends Job implements SelfHandling
 {
+
+    protected $reply_id;
     /**
      * Create a new job instance.
+     * @param $reply_id
      */
-    public function __construct()
+    public function __construct($reply_id)
     {
-        //
+        $this->reply_id = $reply_id;
     }
 
     /**
@@ -20,8 +25,13 @@ class UnLikeReply extends Job implements SelfHandling
      *
      * @return void
      */
-    public function handle()
+    public function handle(LikeRepo $likeRepo, Guard $auth)
     {
-        //
+        $like = $likeRepo->where('user_id', $auth->user()->id)
+                         ->where('reply_id', $this->reply_id)
+                         ->first();
+
+        $like->delete();
+
     }
 }
