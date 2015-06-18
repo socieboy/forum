eeca21a404e448cee642ff9218f34e12071c23f1
Frank <override-chaos@users.noreply.github.com>
2015-06-18 22:19:15 +0100

    Adding the gravatar view

--- src/Views/Partials/avatar.blade.php
+++ src/Views/Partials/avatar.blade.php
@@ -1,9 +1,16 @@
 @if(config('forum.user.avatar'))
+    
+@if(config('forum.user.profile'))
+        <a href="{{ route(config('forum.user.profile-route'), $user->id) }}">
+    @endif
 
-    @if(config('forum.user.profile')) <a href="{{ route(config('forum.user.profile-route'), $user->id) }}"> @endif
-
-        <img class="img-thumbnail img-circle avatar" src="{{ asset($user->{config('forum.user.user-avatar')} ) }}" alt="{{ $user->{config('forum.user.username')} }}"/>
-
+    @if(config('forum.user.gravatar'))
+        <img class="img-thumbnail img-circle avatar" src="{{ sprintf('https://www.gravatar.com/avatar/%s?size=%d', md5($user->email), '200') }}" alt="{{ $user->{config('forum.user.username')} }}"/>
+    @else
+    	<img class="img-thumbnail img-circle avatar" src="{{ asset($user->{config('forum.user.user-avatar')} ) }}" alt="{{ $user->{config('forum.user.username')} }}"/>
+    @endif	
+	
+	
     @if(config('forum.user.profile')) </a> @endif
 
 @endif
