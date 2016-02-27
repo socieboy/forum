372e24f725b8cbdb9c603f765f271f018be3f650
Stygian <stygian@Stygians-MacBook-Pro.local>
2016-02-26 23:35:50 -0600

    Allowing login to redirect back to forum index.

--- src/Views/Partials/login-button.blade.php
+++ src/Views/Partials/login-button.blade.php
@@ -1,4 +1,4 @@
-
+{{ session()->put('url.intended', route('forum')) }}
 <a href="{{ url(config('forum.auth.login-url')) }}" class="btn btn-block btn-success">
     {{ trans('Forum::messages.log-in') }}
 </a>
