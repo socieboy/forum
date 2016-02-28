62de1a2fe98973444b6da58a2467799d93244ef5
Stygian <stygian@Stygians-MacBook-Pro.local>
2016-02-27 22:14:26 -0600

    Fixing en lang

--- src/Lang/en/messages.php
+++ src/Lang/en/messages.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  *
  * Forum translation file.
@@ -8,22 +7,22 @@
  */
 return [
 
-    'forum'                 => 'Forum',
-    'general'               => 'General',
-    'new-conversation'      => 'Start a Conversation',
-    'log-in'                => 'Login In',
-    'search'                => 'Search',
-    'all'                   => 'All',
-    'no-conversations'      => 'There are no conversations for this query',
-    'replies-counter'       => ':replies replies in this conversation',
-    'replies'               => 'reply|replies',
-    'post'                  => 'Post your reply',
-    'post-conversation'     => 'Post conversation',
-    'cancel'                => 'Cancel',
-    'text-like'             => 'Like',
-    'title'                 => 'What\'s your question?',
-    'topic'                 => 'Select a chanel',
-    'message'               => 'Describe your question...',
-    'notification-reply'    => 'left a new reply on your question',
-    'best-answer-asker-choise' => 'Best answer - Asker choise'
+    'forum'                    => 'Forum',
+    'general'                  => 'General',
+    'new-conversation'         => 'Start a Conversation',
+    'log-in'                   => 'Log In',
+    'search'                   => 'Search',
+    'all'                      => 'All',
+    'no-conversations'         => 'There are no conversations for this query',
+    'replies-counter'          => ':replies replies in this conversation',
+    'replies'                  => 'reply|replies',
+    'post'                     => 'Post your reply',
+    'post-conversation'        => 'Post conversation',
+    'cancel'                   => 'Cancel',
+    'text-like'                => 'Like',
+    'title'                    => 'What\'s your question?',
+    'topic'                    => 'Select a channel',
+    'message'                  => 'Describe your question...',
+    'notification-reply'       => 'left a new reply on your question',
+    'best-answer-asker-choise' => 'Best answer - Asker\'s choice',
 ];
