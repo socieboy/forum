e5ddcea67a38422b03aa3817174c7b479d1e3d34
Devonab <alexandre.buruk@gmail.com>
2016-03-17 12:13:28 +0100

    FR translation
    Add french translation

---
+++ src/Lang/fr/messages.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ *
+ * Forum translation file.
+ *
+ */
+return [
+
+    'forum'                    => 'Forum',
+    'general'                  => 'Général',
+    'new-conversation'         => 'Démarrer une nouvelle conversation',
+    'log-in'                   => 'Se connecter',
+    'search'                   => 'Rechercher',
+    'all'                      => 'Tout',
+    'no-conversations'         => 'Aucunes conversations',
+    'replies-counter'          => ':replies réponses dans cette conversation',
+    'replies'                  => 'reply|réponses',
+    'post'                     => 'Poster une réponse',
+    'post-conversation'        => 'Poster la conversation',
+    'cancel'                   => 'Annuler',
+    'text-like'                => 'J\' aime ça',
+    'title'                    => 'Quelle est votre question ?',
+    'topic'                    => 'Sélectionner une catégorie',
+    'message'                  => 'Décrivez votre question...',
+    'notification-reply'       => 'Il a laissé une réponse à votre question',
+    'best-answer-asker-choise' => 'Meilleure - Choisi par le demandeur',
+    'commonmark-support'       => 'est supporté.',
+];
