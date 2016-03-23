9aaf52bbdb4664312f73248046f9641be2323392
Mohamed Sabil <mohamedsabil83@users.noreply.github.com>
2016-03-23 17:05:46 +0300

    Update messages.php

--- src/Lang/ar/messages.php
+++ src/Lang/ar/messages.php
@@ -1,1 +1,27 @@
-
+<?php
+/**
+ *
+ * Forum translation file.
+ *
+ */
+return [
+    'forum'                    => 'المنتدى',
+    'general'                  => 'عام',
+    'new-conversation'         => 'ابدأ محادثة',
+    'log-in'                   => 'دخول',
+    'search'                   => 'بحث',
+    'all'                      => 'الكل',
+    'no-conversations'         => 'لم يتم العثور على محادثات في عملية البحث هذه',
+    'replies-counter'          => ':replies رد في هذه المحادثة',
+    'replies'                  => 'رد|ردود',
+    'post'                     => 'أضف ردك',
+    'post-conversation'        => 'أضف محادثة',
+    'cancel'                   => 'إلغاء',
+    'text-like'                => 'إعجاب',
+    'title'                    => 'ماهو سؤالك ؟',
+    'topic'                    => 'اختر قناة',
+    'message'                  => 'اذكر سؤالك ...',
+    'notification-reply'       => 'اترك رد جديد على سؤالك',
+    'best-answer-asker-choise' => 'أفضل اختيار (حسب السائل)',
+    'commonmark-support'       => 'مدعوم.',
+];
